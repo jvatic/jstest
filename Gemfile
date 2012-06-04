6a2cc5f13252dcbbfe0c09682689f12011e8a1e4
Jesse <jesse@jesses-mac-mini.(none)>
2012-06-03 22:12:00 -0400

    start building automated testing tool for javascript/coffeescript tests

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'haml'
+gem 'sprockets', :git => 'git://github.com/sstephenson/sprockets.git'
+gem 'coffee-script'
+gem 'sass'
---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+Bundler.require
+
+assets = Sprockets::Environment.new do |env|
+  env.logger = Logger.new(STDOUT)
+end
+
+paths = %w{ javascripts stylesheets images }
+paths.each do |path|
+  assets.append_path("public/assets/#{path}")
+end
+
+ARGV.each do |path|
+  if File.exists?(path)
+    assets.append_path(path)
+  end
+end
+
+module AssetHelpers
+  def asset_path(name)
+    "/assets/#{name}"
+  end
+end
+
+assets.context_class.instance_eval do
+  include AssetHelpers
+end
+
+get '/assets/*' do
+  new_env = env.clone
+  new_env["PATH_INFO"].gsub!("/assets", "")
+  assets.call(new_env)
+end
+
+get '/jasmine' do
+  @javascripts = (params[:js_files] || '').split(',')
+  haml :jasmine
+end
+
+puts assets.paths.inspect
